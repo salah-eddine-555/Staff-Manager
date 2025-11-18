4fc41f8673db6ad958007fefe63835700b61331a
unknown <salah-eddine-555>
2025-11-18 14:06:18 +0100

    finaliser l'interface pour les card d'emplyes est les salles

---
+++ scrip.js
@@ -0,0 +1,22 @@
+
+const employers = [
+    {
+        name: "salah-eddine",
+        email: "salah@gmail.com",
+        role: "develeppeur",
+        tele: "+212 623232",
+        photo: 'profile.png'
+        
+    }
+]
+
+function AjouterNoveauEmplyer(a, b){
+    return a + b;  
+}
+
+
+
+
+
+
+
